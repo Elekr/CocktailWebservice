383fd7832887824ab5c7c899ac839146eba793ea
Thomas Crosby <thomascrosby3@gmail.com>
2021-11-29 14:14:13 +0000

    added ingredient

--- src/main/java/com/tsi/tom/crosby/Cocktail_webservice/CocktailWebserviceApplication.java
+++ src/main/java/com/tsi/tom/crosby/Cocktail_webservice/CocktailWebserviceApplication.java
@@ -14,42 +14,75 @@ public class CocktailWebserviceApplication {
 
 	@Autowired
 	private CocktailRepository cocktailRepos;
+	@Autowired
+	private IngredientRepository ingredientRepos;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CocktailWebserviceApplication.class, args);
 	}
 
-	@GetMapping()
-	public String index() {
-		return "is a cocktail menu boiii";
+	//GET
+	@GetMapping("/all/cocktails/Mojito")
+	public @ResponseBody
+	Iterable<Cocktail> getSpecificCocktails() {
+		return cocktailRepos.findAllProjectedBy();
 	}
-	@GetMapping("/all")
-	public @ResponseBody Iterable<Cocktail> getAllCocktails() {
+
+	@GetMapping("/all/cocktails")
+	public @ResponseBody
+	Iterable<Cocktail> getAllCocktails() {
 		return cocktailRepos.findAll();
 	}
 
-    @PostMapping("/addCocktail")
-    public @ResponseBody String addACat (@RequestParam String name, @RequestParam int Cocktail_Instructions_number_of_instructions) {
+	@GetMapping("/all/ingredients")
+	public @ResponseBody
+	Iterable<Ingredient> getAllInstructions() {
+		return ingredientRepos.findAll();
+	}
 
-        Cocktail savedCat = new Cocktail(name, Cocktail_Instructions_number_of_instructions);
-		cocktailRepos.save(savedCat);
-        return "Cocktail saved into list";
+	//POST
+	@PostMapping("/add/cocktail")
+	public @ResponseBody
+	String addCocktail(@RequestParam String name, @RequestParam int Cocktail_Instructions_number_of_instructions) {
 
-    }
+		Cocktail savedDrink = new Cocktail(name, Cocktail_Instructions_number_of_instructions);
+		cocktailRepos.save(savedDrink);
+		return "Cocktail saved into list";
+	}
 
-	@DeleteMapping("deleteCocktail")
-	public @ResponseBody String deleteACocktail(@RequestParam int drink)
-	{
+	@PostMapping("/add/ingredient")
+	public @ResponseBody
+	String addItem(@RequestParam String Name, @RequestParam String Type, Double ABV,
+				   String Description, String Storage_Name) {
+
+		Ingredient item = new Ingredient(Name, Type, ABV, Description, Storage_Name);
+		ingredientRepos.save(item);
+		return "Ingredient saved into list";
+	}
+
+	//DELETE
+	@DeleteMapping("/delete/cocktail")
+	public @ResponseBody
+	String deleteACocktail(@RequestParam int drink) {
 		Optional<Cocktail> cocktail = cocktailRepos.findById(drink);
-		if(cocktail.isEmpty())
-		{
-			return"it ain't workin chief";
-		}
-		else
-		{
+		if (cocktail.isEmpty()) {
+			return "it ain't workin chief";
+		} else {
 			cocktailRepos.deleteById(drink);
-			return"poggers!";
+			return "poggers!";
 		}
 	}
 
+	@DeleteMapping("/delete/ingredient")
+	public @ResponseBody
+	String deleteInstruction(@RequestParam int instr) {
+		Optional<Ingredient> ingredient = ingredientRepos.findById(instr);
+		if (ingredient.isEmpty()) {
+			return "it ain't workin chief but ingredient";
+		} else {
+			ingredientRepos.deleteById(instr);
+			return "poggers! but ingredient";
+		}
+	}
 }
---
+++ src/main/java/com/tsi/tom/crosby/Cocktail_webservice/IngredientRepository.java
@@ -0,0 +1,7 @@
+package com.tsi.tom.crosby.Cocktail_webservice;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface IngredientRepository extends CrudRepository<Ingredient, Integer> {
+
+}
