40242f3078f1d12ff28eaf9ad7aa733b4672ce73
Thomas Crosby <thomascrosby3@gmail.com>
2022-01-05 15:17:44 +0000

    added structure for the testing

---
+++ src/test/java/com/tsi/tom/crosby/Cocktail_webservice/CocktailTest.java
@@ -0,0 +1,13 @@
+package com.tsi.tom.crosby.Cocktail_webservice;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CocktailTest {
+
+    @Test
+    public void testObject()
+    {
+    }
+
+}
---
+++ src/test/java/com/tsi/tom/crosby/Cocktail_webservice/CocktailWebserviceApplicationTest.java
@@ -0,0 +1,12 @@
+package com.tsi.tom.crosby.Cocktail_webservice;
+
+import org.junit.jupiter.api.Test;
+
+public class CocktailWebserviceApplicationTest {
+
+
+    @Test
+    public void testObject()
+    {
+    }
+}
