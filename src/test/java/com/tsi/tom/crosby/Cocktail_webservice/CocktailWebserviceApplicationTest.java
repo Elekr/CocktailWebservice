d257f43c2b88a235f025dba30a69b34561c152e6
Thomas Crosby <thomascrosby3@gmail.com>
2022-01-06 15:15:53 +0000

    holy mama

--- src/test/java/com/tsi/tom/crosby/Cocktail_webservice/CocktailWebserviceApplicationTest.java
+++ src/test/java/com/tsi/tom/crosby/Cocktail_webservice/CocktailWebserviceApplicationTest.java
@@ -1,12 +1,26 @@
 package com.tsi.tom.crosby.Cocktail_webservice;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class CocktailWebserviceApplicationTest {
+
+    @Mock
+    CocktailWebserviceApplication cwa;
 
 
     @Test
-    public void testObject()
+    public void testWebserviceApplication()
     {
+
     }
 }
