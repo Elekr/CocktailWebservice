1b133d51f01a51bf16798584d7ec62ecd6f0663a
Thomas Crosby <thomascrosby3@gmail.com>
2022-01-07 15:07:26 +0000

    added more testing

--- src/test/java/com/tsi/tom/crosby/Cocktail_webservice/CocktailWebserviceApplicationTest.java
+++ src/test/java/com/tsi/tom/crosby/Cocktail_webservice/CocktailWebserviceApplicationTest.java
@@ -1,9 +1,11 @@
 package com.tsi.tom.crosby.Cocktail_webservice;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -13,8 +15,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -39,9 +45,26 @@ public class CocktailWebserviceApplicationTest {
     }
 
     @Test
-    public void testGetAllCocktails()
+    public void testGetAll()
     {
         cocktailWebserviceApplication.getAllCocktails();
         verify(cocktailRepository).findAll();
+        cocktailWebserviceApplication.getAllInstructions();
+        verify(ingredientRepository).findAll();
+        cocktailWebserviceApplication.getAllGlasses();
+        verify(glassRepository).findAll();
+        cocktailWebserviceApplication.getAllEquipment();
+        verify(equipmentRepository).findAll();
     }
+
+    @Test
+    public void testAdd()
+    {
+        String dd = cocktailWebserviceApplication.addCocktail("mojito", 2);
+        ArgumentCaptor<Cocktail> d = ArgumentCaptor.forClass(Cocktail.class);
+        verify(cocktailRepository).save(d.capture());
+        assertEquals("Cocktail saved into list", dd, "The value should be 'Cocktail saved into list'");
+    }
+
+
 }
